797dc8a563ddc8d9008cf6f779ec3725d4cd4bbd
yawadomako123 <adomakoy42@gmail.com>
2026-08-21 17:42:14 +0000

    fix: error banner disappear

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: ['VITE_', 'PAYSTACK_'],
   server: {
     port: 5173,
     open: true,
